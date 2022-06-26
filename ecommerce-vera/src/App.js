40f9bba7e95664c7c1eddcfd0ff855cd12ab6e10
Fernando vera <cesarfernandovera92@gmail.com>
2022-06-25 21:36:39 -0300

    incorporacion de hooks

---
+++ ecommerce-vera/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import ItemListContainer from './components/ItemListConteiner/ItemListenerConteiner';
+import NavBar from './components/NavBar/NavBar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <ItemListContainer/>
+      
+    </div>
+  );
+}
+
+export default App;
